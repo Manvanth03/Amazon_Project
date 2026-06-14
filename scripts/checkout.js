5578ab7660d3cd377fe467b535b57b8ac4c4ea3f
Manvanth <manvanthgolyady03@gmail.com>
2026-06-14 22:49:57 +0530

    Create oop cart Version

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,5 +1,6 @@
 import { renderOrderSummary } from "./checkout/OrderSummary.js";
 import { renderPaymentSummary } from "./checkout/PaymentSummary.js";
+import '../data/cart-oop.js'
 
 renderOrderSummary()
 renderPaymentSummary()
